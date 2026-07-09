ee668f8b3e29ce9d5c721dd0cf0f67d76908b731
saidbekrixsitillayev0-oss <saidbekrixsitillayev0@gmail.com>
2026-07-09 12:09:58 +0500

    Add: Ballni bahoga aylantirish [saidbekrixsitillayev0-oss]

---
+++ lms_kod_topshiriqlari/uy-vazifalari/03-Shartlar_va_Sikllar/01-if___elif___else_____shart_operatorlari_asoslari/01-Ballni_bahoga_aylantirish.py
@@ -0,0 +1,11 @@
+ball = int(input())
+if ball < 0 or ball > 100:
+    print('notogri')
+elif ball >= 86:
+    print(5)
+elif ball >= 71:
+    print(4)
+elif ball >= 56:
+    print(3)
+else:
+    print(2)
