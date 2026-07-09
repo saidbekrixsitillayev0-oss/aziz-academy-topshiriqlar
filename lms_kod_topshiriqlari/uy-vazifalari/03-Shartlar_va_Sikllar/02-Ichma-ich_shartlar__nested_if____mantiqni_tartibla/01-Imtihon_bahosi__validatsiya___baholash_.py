6864882669a5f4ea6417137920a389e7f9d4bc9d
saidbekrixsitillayev0-oss <saidbekrixsitillayev0@gmail.com>
2026-07-09 18:14:19 +0500

    Add: Imtihon bahosi (validatsiya + baholash) [saidbekrixsitillayev0-oss]

---
+++ lms_kod_topshiriqlari/uy-vazifalari/03-Shartlar_va_Sikllar/02-Ichma-ich_shartlar__nested_if____mantiqni_tartibla/01-Imtihon_bahosi__validatsiya___baholash_.py
@@ -0,0 +1,12 @@
+ball = int(input())
+if ball < 0 or ball > 100:
+    print("Notogri ball")
+else:
+    if ball >= 90:
+        print("Alo")
+    elif ball >= 75:
+        print("Yaxshi")
+    elif ball >= 60:
+        print("Qoniqarli")
+    else:
+        print("Yiqildi")
