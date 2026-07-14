69ad3da655210de65d73ce32009fcd1fe7d73df3
saidbekrixsitillayev0-oss <saidbekrixsitillayev0@gmail.com>
2026-07-14 11:06:54 +0500

    Add: Bir necha taxminni baholash [saidbekrixsitillayev0-oss]

---
+++ lms_kod_topshiriqlari/uy-vazifalari/03-Shartlar_va_Sikllar/13-__Loyiha__Taxmin_o_yini__Guess_game____random___wh/01-Bir_necha_taxminni_baholash.py
@@ -0,0 +1,10 @@
+a=int(input())
+k=int(input())
+for i in range(k):
+    x=int(input())
+    if x==a:
+        print("TOPDINGIZ")
+    elif x>a:
+        print("KATTA")
+    else:
+        print("KICHIK")
