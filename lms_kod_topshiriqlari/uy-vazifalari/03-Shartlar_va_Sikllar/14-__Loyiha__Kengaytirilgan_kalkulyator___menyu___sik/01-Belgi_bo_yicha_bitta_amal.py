50d57ca3ccccabf63093bed0cf789a70240d9cc6
saidbekrixsitillayev0-oss <saidbekrixsitillayev0@gmail.com>
2026-07-15 09:40:06 +0500

    Add: Belgi bo'yicha bitta amal [saidbekrixsitillayev0-oss]

---
+++ lms_kod_topshiriqlari/uy-vazifalari/03-Shartlar_va_Sikllar/14-__Loyiha__Kengaytirilgan_kalkulyator___menyu___sik/01-Belgi_bo_yicha_bitta_amal.py
@@ -0,0 +1,13 @@
+a = int(input())
+b = input() .strip()
+c = int(input())
+if b == '+':
+    print(a + c)
+elif b == '-':
+    print(a - b)
+elif b == '*':
+    print(a * c)
+elif b == '/':
+    print(a // c)
+else:
+    print("Noma'lum amal")
