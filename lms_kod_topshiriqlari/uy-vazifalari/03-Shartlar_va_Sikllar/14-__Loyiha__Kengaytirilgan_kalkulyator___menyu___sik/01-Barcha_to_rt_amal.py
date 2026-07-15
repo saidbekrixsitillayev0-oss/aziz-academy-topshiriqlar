9e94aa46f08d21876d59f6005745ea474bfe4266
saidbekrixsitillayev0-oss <saidbekrixsitillayev0@gmail.com>
2026-07-15 09:47:09 +0500

    Add: Barcha to'rt amal [saidbekrixsitillayev0-oss]

---
+++ lms_kod_topshiriqlari/uy-vazifalari/03-Shartlar_va_Sikllar/14-__Loyiha__Kengaytirilgan_kalkulyator___menyu___sik/01-Barcha_to_rt_amal.py
@@ -0,0 +1,9 @@
+a = int(input())
+b = int(input())
+print(f"Yig'indi: {a + b}")
+print(f"Ayirma: {a - b}")
+print(f"Ko'paytma: {a * b}")
+if b == 0:
+    print("Bo'linma: aniqlanmagan")
+else:
+    print(f"Bo'linma: {a // b}")    
